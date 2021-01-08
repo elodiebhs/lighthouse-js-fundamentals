011489a1a2ea0dc9d02443737633c2099cc298a5
elodie <elodiebouthors@hotmail.com>
2021-01-08 00:27:20 +0000

    test

---
+++ test.js
@@ -0,0 +1,16 @@
+const whichSchool  = function age
+
+if (age < 13) {
+  console.log(Elementary School)
+};
+
+else if (age =< 13 && age >=18 ){
+  console.log(Secondary School)
+};
+
+else {
+  console.log(LighthouseLabs)
+};
+  Elementary School if age is below 13
+Secondary School if age is between 13 and 18 (both inclusive)
+Lighthouse Labs in all other cases.
